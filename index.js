69dd46e13f2325624a231c018f556fd11d8dda66
Jayand Bharati <jayand017@gmail.com>
2019-11-23 21:51:39 +0530

    added cors and prefix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 const AuthHandler = require('./src/controller/auth.handler');
 let auth = require('./src/controller/auth.middleware');
 
@@ -7,14 +8,16 @@ let app = express();
 let handler = new AuthHandler();
 
 // Starting point of the server
-
 const AUTH_PORT = process.env.AUTH_PORT || 9001;
+app.use(cors());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
 //Routes & Handlers
-app.post('/register', handler.register);
-app.post('/login', handler.login);
-app.get('/validate', auth.validateToken, handler.dashboard);
+const PREFIX = "/api/auth0/v1";
+app.post(PREFIX + '/register', handler.register);
+app.post(PREFIX + '/login', handler.login);
+app.get(PREFIX + '/validate', auth.validateToken, handler.dashboard);
 app.listen(AUTH_PORT, () => console.log(`Server is listening on port: ${AUTH_PORT}`))
